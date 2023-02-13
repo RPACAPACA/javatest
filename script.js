890b47aa61b5efb8659686f0b7e4ced43a0a1328
RPACAPACA <125248938+RPACAPACA@users.noreply.github.com>
2023-02-13 16:55:10 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("scroll-button").addEventListener("click", function() {
-  document.getElementById("next-page").style.display = "block";
-  window.scroll({
-    top: window.innerHeight, 
-    behavior: "smooth"
-  });
-});
